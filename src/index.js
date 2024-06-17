45fe5a96b0243cb848ee0370b226f42aebff36d9
Rosay87 <rosa.b.yilmaz@gmail.com>
2024-06-17 16:56:29 +1000

    Axios added & Search function built

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+  let temperatureELement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureELement.innerHTML = response.data.temperature.current;
+  temperatureELement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a448f164ed4ot600b779c438c2f1cdaf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", handleSearchSubmit);
