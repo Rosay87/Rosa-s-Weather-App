f76184c06cd1f3a57989f06d80c44b212d3caea3
Rosay87 <rosa.b.yilmaz@gmail.com>
2024-06-17 17:44:57 +1000

    Added weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,42 @@ function refreshWeather(response) {
   let temperatureELement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#wind-speed");
+  let imageElement = document.querySelector("#weather-image");
+  let timeElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   temperatureELement.innerHTML = response.data.temperature.current;
   temperatureELement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  speedElement.innerHTML = response.data.wind.speed;
+  imageElement.innerHTML = response.data.condition.icon_url;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
